220ba6fadd8446a00592ed8adec607279418cbaf
ShekharGupta1988 <shgup@microsoft.com>
2018-01-03 16:55:52 -0800

    skeleton test project

---
+++ Diagnostics/Diagnostics.Tests/ScriptsTests/CompilationServiceTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Diagnostics.Tests.ScriptsTests
+{
+    public class CompilationServiceTests
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ Diagnostics/Diagnostics.Tests/ScriptsTests/CompilationTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Diagnostics.Tests.ScriptsTests
+{
+    public class CompilationTests
+    {
+
+    }
+}
