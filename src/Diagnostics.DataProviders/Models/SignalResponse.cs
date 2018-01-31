17f9b6ad024d1522403e43dadc6e033687ac3709
stevenernst131 <sterns@microsoft.com>
2018-01-31 11:36:46 -0800

    Adding Basic Signal Model

---
+++ src/Diagnostics.DataProviders/Models/SignalResponse.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Diagnostics.DataProviders.Models
+{
+    class SignalResponse
+    {
+        public DataTableResponseObject Data { get; set; }
+
+        public SignalMetaData SignalMetaData { get; set; }
+
+        public RenderingDefinition RenderingDefinition { get; set; }
+
+        public SignalResponse()
+        {
+            Data = new DataTableResponseObject();
+            SignalMetaData = new SignalMetaData();
+        }
+    }
+
+    public class SignalMetaData
+    {
+        public string Id { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public string Description { get; set; }
+    }
+
+    public class RenderingDefinition
+    {
+        public SignalGraphType SignalGraphType { get; set; }
+    }
+
+    public enum SignalGraphType
+    {
+        NoGraph = 0,
+        Table,
+        TimeSeries
+    }
+}
