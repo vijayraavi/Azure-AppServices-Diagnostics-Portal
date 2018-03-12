f30f5f274497ea16a37ad82bb7449069f18bf5a8
ShekharGupta1988 <shgup@microsoft.com>
2018-03-11 23:37:06 -0700

    update build script

--- build.cmd
+++ build.cmd
@@ -13,7 +13,7 @@ exit /b %errorlevel%
 
 :: Publish Compiler Host to Build Location
 echo "Publishing Compiler Host to build directory"
-dotnet publish src\\Diagnostics.CompilerHost\\Diagnostics.CompilerHost.csproj -c Release -o ..\\..\\build\\Diagnostics.CompilerHost.1.0
+dotnet publish src\\Diagnostics.CompilerHost\\Diagnostics.CompilerHost.csproj -c Release -o ..\\..\\build\\antares.external.diagnostics.compilerhost.1.0.0
 
 IF %ERRORLEVEL% NEQ 0 (
 echo "Diagnostics.CompilerHost Publish Failed."
@@ -22,7 +22,7 @@ exit /b %errorlevel%
 
 :: Publish Runtime Host to Build Location
 echo "Publishing Runtime Host to build directory"
-dotnet publish src\\Diagnostics.RuntimeHost\\Diagnostics.RuntimeHost.csproj -c Release -o ..\\..\\build\\Diagnostics.RuntimeHost.1.0
+dotnet publish src\\Diagnostics.RuntimeHost\\Diagnostics.RuntimeHost.csproj -c Release -o ..\\..\\build\\antares.external.diagnostics.runtimehost.1.0.0
 
 IF %ERRORLEVEL% NEQ 0 (
 echo "Diagnostics.RuntimeHost Publish Failed."
