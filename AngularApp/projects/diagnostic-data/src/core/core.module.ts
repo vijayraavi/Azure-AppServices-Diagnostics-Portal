8906a36780e219a954f46bed6fee061e5658f1fd
Daniel Grooms <daniel.grooms@microsoft.com>
2019-03-13 11:41:26 -0700

    Create core module and generate vertical nav component

---
+++ AngularApp/projects/diagnostic-data/src/core/core.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { VerticalNavComponent } from './vertical-nav/vertical-nav.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [VerticalNavComponent]
+})
+export class CoreModule { }
---
+++ AngularApp/projects/diagnostic-data/src/core/vertical-nav/vertical-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'vertical-nav',
+  templateUrl: './vertical-nav.component.html',
+  styleUrls: ['./vertical-nav.component.scss']
+})
+export class VerticalNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
