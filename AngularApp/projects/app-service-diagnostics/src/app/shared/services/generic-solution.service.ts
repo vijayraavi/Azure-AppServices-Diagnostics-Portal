c60346a2e57507af74b6e91a38b0762af73d8397
Daniel Grooms <daniel.grooms@microsoft.com>
2019-03-22 11:48:13 -0700

    GotoBlade and OpenTab actions work from solution

--- AngularApp/projects/app-service-diagnostics/src/app/shared/services/generic-solution.service.ts
+++ AngularApp/projects/app-service-diagnostics/src/app/shared/services/generic-solution.service.ts
@@ -1,17 +1,20 @@
 import { TelemetryService } from 'diagnostic-data';
 import { Observable, of } from 'rxjs';
 import { Injectable } from '@angular/core';
-import { DefaultUrlSerializer } from '@angular/router';
+import { DefaultUrlSerializer, Router } from '@angular/router';
 import { PortalService } from '../../startup/services/portal.service';
 import { OpenBladeInfo } from '../models/portal';
 import { ArmService } from './arm.service';
+import { PortalActionService } from './portal-action.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GenericSolutionService {
 
-  constructor(private armService: ArmService, private portalService: PortalService, private logService: TelemetryService) {}
+  constructor(private armService: ArmService, private portalService: PortalService,
+    private logService: TelemetryService, private portalNavService: PortalActionService,
+    private _router: Router) {}
 
   validateArmApiOptions(options: {}) {
     // Check for keys: route, verb, body (optional)
@@ -47,24 +50,37 @@ export class GenericSolutionService {
     return this.armService[actionMethod](route, body, true, apiVersion);
   }
 
-  OpenTab(resourceUri: string, actionOptions: {}): Observable<any> {
+  OpenTab(resourceUri: string, actionOptions: {tabUrl: string}): Observable<any> {
     this.logService.logEvent('solution_open_tab', {'resourceUri': resourceUri, ...actionOptions});
-    return of("Not implemented");
+
+    if (!('tabUrl' in actionOptions)) {
+      throw new Error('ActionOptions should include the tabUrl property');
+    }
+
+    const tabUrl = actionOptions['tabUrl'].toLowerCase();
+    this._router.navigateByUrl(tabUrl);
+
+    return of(tabUrl);
   }
 
   getBladeInfo(options: {}) {
     if (!('detailBlade' in options)) {
       throw new Error('ActionOptions should include the detailBlade property');
     }
+
     return options as OpenBladeInfo;
   }
 
-  GoToBlade(resourceUri: string, actionOptions: {}): Observable<any> {
+  GoToBlade(resourceUri: string, actionOptions: {detailBlade: string}): Observable<any> {
     this.logService.logEvent('solution_goto_blade', {'resourceUri': resourceUri, ...actionOptions});
 
     const bladeInfo = this.getBladeInfo(actionOptions);
 
-    this.portalService.openBlade(bladeInfo, 'troubleshoot');
+    if (bladeInfo.detailBlade === 'scaleup') {
+      this.portalNavService.openBladeScaleUpBlade();
+    } else {
+      this.portalService.openBlade(bladeInfo, 'troubleshoot');
+    }
 
     return of(bladeInfo.detailBlade);
   }
