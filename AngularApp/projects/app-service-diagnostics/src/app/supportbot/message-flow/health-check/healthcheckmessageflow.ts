f4dcd3315b8d81582fb55bf73bee152bd7624010
Puneet Gupta <puneetg@microsoft.com>
2019-06-21 16:54:33 +0530

    Removing v3 Detectors and enabling HealthCheck for v3

--- AngularApp/projects/app-service-diagnostics/src/app/supportbot/message-flow/health-check/healthcheckmessageflow.ts
+++ AngularApp/projects/app-service-diagnostics/src/app/supportbot/message-flow/health-check/healthcheckmessageflow.ts
@@ -90,12 +90,12 @@ export class HealthCheckMessageFlow extends IMessageFlowProvider {
 
 export class HealthCheckMessage extends Message {
     constructor() {
-        super(HealthCheckComponent, {});
+        //super(HealthCheckComponent, {});
 
         //
         // Comment above line and uncomment the below line
         // to enable the applensv3 HealthCheck component
         //
-        //super(HealthCheckV3Component, {});
+        super(HealthCheckV3Component, {});
     }
 }
