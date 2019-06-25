b6e60a1a79dc05ffa420c9ed7cce6c18dd7c1d4f
Puneet Gupta <puneetg@microsoft.com>
2019-06-25 12:37:40 +0530

    Ability to specify an order for category tiles

--- AngularApp/projects/app-service-diagnostics/src/app/shared-v2/services/feature.service.ts
+++ AngularApp/projects/app-service-diagnostics/src/app/shared-v2/services/feature.service.ts
@@ -6,6 +6,9 @@ import { ContentService } from './content.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../../startup/services/auth.service';
 import { LoggingV2Service } from './logging-v2.service';
+import { OperatingSystem } from '../../shared/models/site';
+import { WebSitesService } from '../../resources/web-sites/services/web-sites.service';
+import { AppType } from '../../shared/models/portal';
 
 @Injectable()
 export class FeatureService {
@@ -13,38 +16,47 @@ export class FeatureService {
   public detectors: DetectorMetaData[];
 
   protected _features: Feature[] = [];
+  protected _featureDisplayOrder = [];
 
   constructor(protected _diagnosticApiService: DiagnosticService, protected _contentService: ContentService, protected _router: Router, protected _authService: AuthService,
-    protected _logger: LoggingV2Service) {
-    this._authService.getStartupInfo().subscribe(startupInfo => {
+    protected _logger: LoggingV2Service, private _resourceService: WebSitesService) {
 
+    this._featureDisplayOrder = [{
+      category: "Availability and Performance",
+      platform: OperatingSystem.windows,
+      appType: AppType.WebApp,
+      order: ['Web App Down', 'Web App Slow', 'High CPU Analysis', 'Memory Analysis', 'Web App Restarted'].reverse()
+    }];
+
+    this._authService.getStartupInfo().subscribe(startupInfo => {
       this._diagnosticApiService.getDetectors().subscribe(detectors => {
         detectors.forEach(detector => {
-          if (detector.type === DetectorType.Detector){
-
-          this._features.push(<Feature>{
-            id: detector.id,
-            description: detector.description,
-            category: detector.category,
-            featureType: FeatureTypes.Detector,
-            name: detector.name,
-            clickAction: this._createFeatureAction(detector.name, detector.category, () => {
-              this._router.navigateByUrl(`resource${startupInfo.resourceId}/detectors/${detector.id}`);
-            })
-          });
-        } else {
-          this._features.push(<Feature>{
-            id: detector.id,
-            description: detector.description,
-            category: detector.category,
-            featureType: FeatureTypes.Detector,
-            name: detector.name,
-            clickAction: this._createFeatureAction(detector.name, detector.category, () => {
-              this._router.navigateByUrl(`resource${startupInfo.resourceId}/analysis/${detector.id}`);
-            })
-          });
-        }
+          if (detector.type === DetectorType.Detector) {
+            this._features.push(<Feature>{
+              id: detector.id,
+              description: detector.description,
+              category: detector.category,
+              featureType: FeatureTypes.Detector,
+              name: detector.name,
+              clickAction: this._createFeatureAction(detector.name, detector.category, () => {
+                this._router.navigateByUrl(`resource${startupInfo.resourceId}/detectors/${detector.id}`);
+              })
+            });
+          } else {
+            this._features.push(<Feature>{
+              id: detector.id,
+              description: detector.description,
+              category: detector.category,
+              featureType: FeatureTypes.Detector,
+              name: detector.name,
+              clickAction: this._createFeatureAction(detector.name, detector.category, () => {
+                this._router.navigateByUrl(`resource${startupInfo.resourceId}/analysis/${detector.id}`);
+              })
+            });
+          }
         });
+
+        this.sortFeatures();
       });
 
       this._contentService.getContent().subscribe(articles => {
@@ -61,6 +73,49 @@ export class FeatureService {
           });
         });
       });
+    });
+  }
+
+  sortFeatures() {
+    let featureDisplayOrder = this._featureDisplayOrder;
+
+    featureDisplayOrder.forEach(feature => {
+
+      // Add all the features for this category to a temporary array
+      let categoryFeatures: Feature[] = [];
+      this._features.forEach(x => {
+        if (x.category != null && x.category.indexOf(feature.category) > -1 && feature.platform === this._resourceService.platform && this._resourceService.appType === feature.appType) {
+          categoryFeatures.push(x);
+        }
+      });
+
+      // Remove all the features for the sorted category
+      if (feature.platform === this._resourceService.platform && this._resourceService.appType === feature.appType) {
+        this._features = this._features.filter(x => {
+          return x.category !== feature.category;
+        });
+      }
+
+      // Sort all the features for this category
+      categoryFeatures.sort(
+        function (a, b) {
+          let categoryOrder = featureDisplayOrder.find(x => x.category.toLowerCase().startsWith(feature.category.toLowerCase()));
+          if (categoryOrder != null) {
+            if (categoryOrder.order.indexOf(a.name) < categoryOrder.order.indexOf(b.name)) {
+              return 1;
+            } else if (categoryOrder.order.indexOf(b.name) === categoryOrder.order.indexOf(a.name)) {
+              return 0;
+            }
+            else {
+              return -1;
+            }
+          }
+        }
+      );
+
+      // add the sorted features for this category back to the array
+      this._features = this._features.concat(categoryFeatures);
+
     });
   }
 
