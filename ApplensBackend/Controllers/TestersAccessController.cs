7f2646c1d3c85bdc2cfeb4c69f3b7774798cd93d
Ajay Sharma <ajsharm@microsoft.com>
2019-07-11 18:09:35 -0700

    Comments for summary

--- ApplensBackend/Controllers/TestersAccessController.cs
+++ ApplensBackend/Controllers/TestersAccessController.cs
@@ -3,11 +3,18 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AppLensV3.Controllers
 {
+    /// <summary>
+    /// Testers Access controller.
+    /// </summary>
     [Route("api/hasTestersAccess")]
     [Authorize(Policy = "ApplensAccess")]
     [Authorize(Policy = "ApplensTesters")]
     public class TestersAccessController : Controller
     {
+        /// <summary>
+        /// Method to check testers access.
+        /// </summary>
+        /// <returns>True result.</returns>
         [HttpGet("")]
         [HttpOptions("")]
         public IActionResult CheckTestersAccess(){
